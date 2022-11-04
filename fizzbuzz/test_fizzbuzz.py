1939e93805539d9ea2d08556e22e4e3b86194152
Daniel Moy <daniel.moy@ocado.com>
2022-11-04 14:29:04 -0400

    fizzbuzz kata

---
+++ fizzbuzz/test_fizzbuzz.py
@@ -0,0 +1,14 @@
+import unittest
+import typing
+import re
+
+PASSWORD = "password12"
+
+
+class TestPassword(unittest.TestCase):
+    def test_fizzbuzz_1(self):
+        pass
+
+
+def transform_to_fizzbuzz():
+    pass
